b827f82aeb092dd32223c4906b23aed25d7bef6e
Diego Menegazzi <omenegazzi@gmail.com>
2018-12-25 21:54:51 -0200

    Criado o Model Xml e criado os atributos juntamente com os gets e sets.

--- src/mini/projeto/main.java
+++ src/mini/projeto/main.java
@@ -3,7 +3,6 @@ package mini.projeto;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/model/Xml.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Xml {
+
+	private String title;
+	private String pubDate;
+	private String enclosure;
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public String getEnclosure() {
+		return enclosure;
+	}
+
+	public void setEnclosure(String enclosure) {
+		this.enclosure = enclosure;
+	}
+
+}
