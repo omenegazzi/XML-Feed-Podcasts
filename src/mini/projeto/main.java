fff9eefdeb183e8c64cb195c92f806953c6d8e88
Diego Menegazzi <omenegazzi@gmail.com>
2018-12-26 16:13:56 -0200

    Implementado código para fazer download de quantidade de episódios.

--- src/mini/projeto/main.java
+++ src/mini/projeto/main.java
@@ -1,6 +1,12 @@
 package mini.projeto;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,9 +28,10 @@ public class main {
 
 		Scanner in = new Scanner(System.in);
 
-		Runnable runnable = () -> {
+		Runnable runnableMain = () -> {
 
 			try {
+
 				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
@@ -76,7 +83,7 @@ public class main {
 						System.out.printf("Você digitou uma operação inválida.");
 					}
 
-					DownloadEpisodios();
+					DownloadEpisodios(xml);
 
 				}
 
@@ -88,23 +95,67 @@ public class main {
 				e.printStackTrace();
 			} catch (IOException e) {
 				System.out.println("ATENÇÃO ERRO! Endereço de feed Inválido! \n");
-				e.printStackTrace();
+
+				// main.main(args);
+				// e.printStackTrace();
 			}
 
 		};
 
-		new Thread(runnable).start();
-
+		new Thread(runnableMain).start();
 	}
 
-	public static void DownloadEpisodios() {
+	public static void DownloadEpisodios(ArrayList<Xml> xml) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Digite a quantidade de episódios para baixar: \n");
 		int quant = in.nextInt();
+
+		ArrayList<String> xmlDownload = new ArrayList();
+
+		for (int i = 0; i < quant; i++) {
+			xmlDownload.add(xml.get(i).getEnclosure());
+		}
+
+		Runnable runnableDownload = () -> {
+			try {
+
+				for (int i = 0; i < xmlDownload.size(); i++) {
+
+					URLConnection conn = new URL(xmlDownload.get(i).toString()).openConnection();
+					InputStream is = conn.getInputStream();
+
+					String[] result = xmlDownload.get(i).toString().split("\\/");
+
+					OutputStream outstream = new FileOutputStream(new File("src/" + result[9]));
+					byte[] buffer = new byte[4096];
+					int len;
+					System.out.println("AGUARDE...! Baixando episódio " + result[9]);
+					while ((len = is.read(buffer)) > 0) {
+						outstream.write(buffer, 0, len);
+					}
+					outstream.close();
+					System.out.println("Episódio Baixado!");
+
+				}
+
+			} catch (IOException e) {
+
+				System.out.println("Não foi possivel baixar o arquivo");
+			}
+		};
+
+		new Thread(runnableDownload).start();
+
+		/*
+		 * while(!quant.substring(0).matches("[0-9]*")) {
+		 * System.out.println("Somente é aceito números!");
+		 * System.out.println("Digite a quantidade de episódios para baixar: \n"); quant
+		 * = in.nextLine(); }
+		 */
+
 	}
 
 	public static void BuscarEpisodios() {
 
 	}
-
 }
